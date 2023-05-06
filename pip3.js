0d609e69da0ae4aab3f32aa07e588d8344301c1d
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-05-06 13:57:44 +0300

    first commit

---
+++ pip3.js
@@ -0,0 +1,53 @@
+//Given an array of strings, use a function to reverse all the elements in the string in ascending order and the specific elements in descending order
+function array_of_string(element)
+
+
+
+
+
+
+
+
+
+
+
+
+//Given an array of objects, each object representing a person with a name and age property, write a function that returns the sum of all people who are less than 18 years.
+const people = [
+  { name: 'Alice', age: 17 },
+  { name: 'Eunice', age: 22 },
+  { name: 'Charlie', age: 14 },
+  { name: 'Max', age: 19 },
+];
+const Adult = (people)=>{
+    return people.filter(people =>personalbar.age>=18)
+    .map(person=>person.name)
+
+}
+
+console .log (Adult);
+
+
+
+
+//Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
+const sumPositive = (numbers) => {
+let elements  = [0,1-2.3-4.5.];
+if (element=(postive)) 
+return postive
+}
+else{
+    return(zero);
+}
+
+
+//Given an array of objects, where each object represents an employee with an id, name, and salary property, write a function that returns a new array of employee objects sorted by their salary in ascending order.
+const employee =
+{name:"Janet",id:"Tipanoi Shirin",salary:5,000}
+{name:"Ann",id:"Kamondo Hillary",salary:4,000}
+{name:"Yvonne",id:"Adhiambo Akinyi",salary:25,000}
+
+const array(properties)=>{
+    return array.sorted(descending)
+}
+
